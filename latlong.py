0d92b948e78b81b8c41126d3b172a9f15461af00
George Shao <GeorgeShao123@gmail.com>
2019-10-19 13:03:54 -0400

    Got latitude & longitude data from metadata file.

--- latlong.py
+++ latlong.py
@@ -1,16 +1,20 @@
 import os
+from checkFileExists import checkFileExists
+
 def returnlatlong(file_name):
     location_file = f"{file_name}_InfoMetadata"
 
     file = "./PartialDataset/"+file_name+"/"+location_file+".txt"
 
-    meta = open(file,"r+")
+    if checkFileExists(file):
+        meta = open(file,"r+")
 
     list2 = meta.readlines()
     latitude = list2[19][10:]
     longitude = list2[20][12:]
+    latitude=float(latitude)
+    longitude=float(longitude)
+    return [latitude,longitude]
 
-
-    return [list2[19],list2[20]]
-
-print(returnlatlong(sr))
+    
+print(returnlatlong("sr7933"))
