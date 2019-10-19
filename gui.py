10e2c1cecd15bcf22661de087954a0c98b0e726c
George Shao <GeorgeShao123@gmail.com>
2019-10-19 19:02:57 -0400

    Co-authored-by: daythekid <dhyey.p.mehta@gmail.com> Co-authored-by: Frank Ye <frankfrankfrankyeyeye@gmail.com> Added matplotlib graph with 2 y axis.

--- gui.py
+++ gui.py
@@ -2,14 +2,18 @@ import matplotlib.pyplot as plt
 from main import DATA
 import numpy as np
 
+fig, ax1 = plt.subplots()
+
 for i in range(len(DATA) // 2):
     #master_ozone.append()
     #master_acid.append()
 
+    ax1.scatter(list(range(1, 151)), DATA[i*2], color='blue', s=1) #ozone
+    ax2 = ax1.twinx()
+    ax2.scatter(list(range(1, 151)), DATA[(i*2)+1], color='red', s=1) #Hydrochloric acid
 
-    plt.scatter(list(range(1, 151)), DATA[i*2], color='blue') #ozone
-    plt.scatter(list(range(1, 151)), DATA[(i*2)+1], color='red') #Hydrochloric acid
 
-plt.xlabel('Elevation (km)')
-plt.ylabel('Concentration (ppm)')
+ax1.set_xlabel('Elevation (km)')
+ax1.set_ylabel('Concentration (ppm)')
+ax2.set_ylabel('Concentration (ppm)')
 plt.show()
