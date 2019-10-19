52f6b4439e7870be2d5c37aa03824402c159de8c
George Shao <GeorgeShao123@gmail.com>
2019-10-19 13:04:27 -0400

    Got Ozone & HCl data from local source.

--- parse_data.py
+++ parse_data.py
@@ -1,24 +1,20 @@
-def checkFileExists(file_name):
-    if os.path.isfile(f'{file_name}'):
-        print(f'File {file_name} exists')
-        return True
-    else:
-        print(f'ERROR: File {file_name} does not exist')
-        raise FileNotFoundError
-        return False
+import os
+from checkFileExists import checkFileExists
 
-
-if checkFileExists('./PartialDataset/sr7933/Data-L2_1km_grid/O3.csv'):
-    with open('./PartialDataset/sr7933/Data-L2_1km_grid/O3.csv') as f:
+file = './PartialDataset/sr7933/Data-L2_1km_grid/O3.csv'
+if checkFileExists(file):
+    with open(file) as f:
         ozone_data = f.readlines()
 
-if checkFileExists('./PartialDataset/sr7933/Data-L2_1km_grid/HCl.csv'):
-    with open('./PartialDataset/sr7933/Data-L2_1km_grid/HCl.csv') as f:
+file = './PartialDataset/sr7933/Data-L2_1km_grid/HCl.csv'
+if checkFileExists(file):
+    with open(file) as f:
         acid_data = f.readlines()
 
 print(ozone_data, acid_data)
 
 ozone_data, acid_data = list(map(float, ozone_data)), list(map(float, acid_data))
 
-print(ozone_data, acid_data)
+ozone_data, acid_data = [None if x == -999.0 else x for x in ozone_data], [None if x == -999.0 else x for x in acid_data]
 
+print(ozone_data, acid_data)
