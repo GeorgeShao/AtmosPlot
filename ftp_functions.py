ca6c196667ab78a35e89d9d8087c01c65ccc7ec2
George Shao <GeorgeShao123@gmail.com>
2019-10-19 13:33:47 -0400

    Added ftp_functions.py

--- ftp_functions.py
+++ ftp_functions.py
@@ -1,1 +1,13 @@
 from ftplib import FTP
+import os
+
+
+ftp = FTP('ftp.asc-csa.gc.ca')
+ftp.login()
+
+def access(path_to_file: str):
+    path_to_file = os.path.normpath(path).split(os.sep)
+    return_string = str()
+    BASE = "users/OpenData_DonneesOuvertes/pub/SCISAT/Data_format CSV/"
+    ftp.retrlines(f'RETR {base}{path_to_file}', lambda x: return_string.append(x))
+    return return_string
