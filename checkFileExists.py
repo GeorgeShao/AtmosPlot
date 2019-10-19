11db6fd4ebb0e3cc0dca13bae4fc27c3ae687743
George Shao <GeorgeShao123@gmail.com>
2019-10-19 13:59:47 -0400

    removed log printing for checkFileExists function

--- checkFileExists.py
+++ checkFileExists.py
@@ -2,7 +2,6 @@ import os
 
 def checkFileExists(file_name):
     if os.path.isfile(f'{file_name}'):
-        print(f'File {file_name} exists')
         return True
     else:
         print(f'ERROR: File {file_name} does not exist')
