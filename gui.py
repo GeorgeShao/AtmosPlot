d642fae96b0b96eeeabaec001d17ba4ea9cfd1a5
George Shao <GeorgeShao123@gmail.com>
2019-10-19 22:38:56 -0400

    Fixed negative concentration problem partially.

--- gui.py
+++ gui.py
@@ -6,15 +6,15 @@ import numpy as np
 fig, ax1 = plt.subplots()
 DATA_POINT_SPLIT = 3
 
+ax2 = ax1.twinx()
 
 for i in range(len(DATA) // DATA_POINT_SPLIT):
     coords = DATA[i*DATA_POINT_SPLIT+2]
     red_amount = (coords[0] + 90) / 180
     blue_amount = (coords[2] + 180) / 360
     # print(red_amount, blue_amount)
-    ax1.scatter(list(range(1, 151)), DATA[i*DATA_POINT_SPLIT], facecolor=matplotlib.colors.to_hex((red_amount, 0, blue_amount),keep_alpha=False), s=1, marker='.')  # ozone
-    ax2 = ax1.twinx()
-    ax2.scatter(list(range(1, 151)), DATA[(i*DATA_POINT_SPLIT)+1], facecolor=matplotlib.colors.to_hex((red_amount, 1, blue_amount),keep_alpha=False), s=1, marker='v')  # hydrochloric acid
+    ax1.scatter(list(range(1, 151)), DATA[i*DATA_POINT_SPLIT], facecolor=matplotlib.colors.to_hex((red_amount, 0, blue_amount),keep_alpha=False), s=1, marker=',')  # ozone
+    ax2.scatter(list(range(1, 151)), DATA[(i*DATA_POINT_SPLIT)+1], facecolor=matplotlib.colors.to_hex((red_amount, 1, blue_amount),keep_alpha=False), s=1, marker=',')  # hydrochloric acid
 
 
 ax1.set_xlabel('Elevation (km)')
